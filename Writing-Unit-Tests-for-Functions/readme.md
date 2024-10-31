7dff952133a9abed49c20bc762273b6ccbfc7037
adarsh-gupta101 <adarshgupta2468@gmail.com>
2024-10-31 11:17:37 +0530

    simple unit test for a calculator

---
+++ Writing-Unit-Tests-for-Functions/readme.md
@@ -0,0 +1,20 @@
+<!-- include images from current folder -->
+
+
+## Writing Unit Tests for Functions
+
+In this, you will write unit tests for a function that mimics a calculator
+
+### Instructions
+
+1. npm install jasmine --dev
+
+2. npx jasmine init
+
+3. npm test
+
+### Screenshots
+
+![screenshot](./screenshot-1.png)
+![screenshot2](./screenshot-2.png)
+
---
+++ Writing-Unit-Tests-for-Functions/spec/support/calculator_function_test.spec.js
@@ -0,0 +1,42 @@
+
+const calculator = require('../../calculator');
+
+describe("Calculator Function Tests", () => {
+  describe("Addition", () => {
+    it("should correctly add two positive numbers", () => {
+      expect(calculator.add(5, 7)).toBe(12);
+    });
+
+    it("should correctly add negative numbers", () => {
+      expect(calculator.add(-3, -6)).toBe(-9);
+    });
+  });
+
+  describe("Subtraction", () => {
+    it("should correctly subtract two numbers", () => {
+      expect(calculator.subtract(10, 3)).toBe(7);
+    });
+  });
+
+  describe("Multiplication", () => {
+    it("should correctly multiply two numbers", () => {
+      expect(calculator.multiply(4, 5)).toBe(20);
+    });
+  });
+
+  describe("Division", () => {
+    it("should correctly divide two numbers", () => {
+      expect(calculator.divide(10, 2)).toBe(5);
+    });
+
+    it("should throw an error when dividing by zero", () => {
+      expect(() => calculator.divide(10, 0)).toThrowError("Cannot divide by zero");
+    });
+  });
+
+  describe("Percentage", () => {
+    it("should calculate the correct percentage", () => {
+      expect(calculator.percentage(20, 200)).toBeCloseTo(10);
+    });
+  });
+});
